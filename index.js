04a5e77ed01717c556b8474ec4e465fe6c1bb239
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-11 18:01:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ function addToCart(item) {
  // write your code here
 }
 
-function viewCart() {
-  // write your code here
-}
-
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
+// function viewCart() {
+//   // write your code here
+// }
+// 
+// function total() {
+//   // write your code here
+// }
+// 
+// function removeFromCart(item) {
+//   // write your code here
+// }
+// 
+// function placeOrder(cardNumber) {
+//   // write your code here
+// }
