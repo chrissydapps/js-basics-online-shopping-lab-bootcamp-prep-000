20521cb27191abad2b9446013e9a54eb70fb2a4c
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-11 18:02:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ function addToCart(item) {
 // function viewCart() {
 //   // write your code here
 // }
-// 
+//
 // function total() {
 //   // write your code here
 // }
-// 
+//
 // function removeFromCart(item) {
 //   // write your code here
 // }
-// 
+//
 // function placeOrder(cardNumber) {
 //   // write your code here
 // }
